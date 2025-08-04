a0ed74799094ada9fc5e876a802d29ceca7580e4
DiegoBrine269 <diegooloarte269@gmail.com>
2025-08-04 15:33:22 -0600

    Ajuste en clase de modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ const Modal = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 z-10 flex justify-center items-start sm:items-center bg-black/75 overflow-y-auto py-10 ">
+        <div className="fixed inset-0 z-10 flex justify-center items-start  bg-black/75 overflow-y-auto py-10 ">
             <div className="bg-white rounded-2xl shadow-lg p-6 max-w-lg w-full relative mx-2 dark:bg-neutral-800 animate__animated animate__animated animate__fadeIn">
                 <button
                     onClick={onClose}
