c79a03703d56c57cd151599c2df23377b0ab1324
DiegoBrine269 <diegooloarte269@gmail.com>
2025-10-30 18:36:40 -0600

    Bug agregar vehiculo

--- src/Pages/Proyectos/Proyecto/Proyecto.jsx
+++ src/Pages/Proyectos/Proyecto/Proyecto.jsx
@@ -156,6 +156,7 @@ export default function Proyecto() {
                 setErrors(error.response.data.errors);
             }
         } finally {
+            setLoading(false)
             formFiltrosRef.reset();
             tableRef.current.clearFilter();
         }
