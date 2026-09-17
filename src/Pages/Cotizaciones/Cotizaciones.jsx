8efc396133e447b0615e797d29460c31a586aed8
DiegoBrine269 <diegooloarte269@gmail.com>
2026-09-17 14:27:44 -0600

    Disable row selection in Cotizaciones table options for improved user experience

--- src/Pages/Cotizaciones/Cotizaciones.jsx
+++ src/Pages/Cotizaciones/Cotizaciones.jsx
@@ -750,7 +750,7 @@ export default function Cotizaciones() {
                     ref={tableRef}
                     layout="fitColumns"
                     options={{
-                        selectable: true,
+                        // selectable: true,
                         selectablePersistence: true,
                         pagination: true, //enable pagination
                         paginationMode: "remote", //enable remote pagination
